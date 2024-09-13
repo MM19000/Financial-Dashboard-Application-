06c66377cd6d868efd643bab0011db99e99342f3
MM19000 <ka897976@gmail.com>
2024-09-14 00:30:00 +0300

    my app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+// import InvoiceStatus from "./ui/invoices/status"
+// import { lusitana } from '../app/ui/fonts.js';
+// import Layout from './layout';
+// import Image from "next/image";
+// import heroImage from "./hero-mobile.png";
+export default function Home() {
+  return (
+
+      <>Home Page</>
+  );
+}
